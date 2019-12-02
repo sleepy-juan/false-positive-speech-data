985bf91b837554d73bda5fe1d6b7ca72c55f5570
Taeo <taeohy@kaist.ac.kr>
2019-12-02 23:04:12 +0900

    Add Plan B
    Implement same functions by python to prepare the case of failure

---
+++ PlanB/EditWave.py
@@ -0,0 +1,15 @@
+import wave
+
+try:
+    orginal = wave.open('weather.wav', 'rb')
+    modified = wave.open('modified.wav', 'wb')
+
+    nchannels, sampwidth, framerate, nframes, comptype, compname = orginal.getparams()
+    modified.setparams((nchannels, sampwidth, framerate, nframes, comptype, compname))
+
+    original_frames = orginal.readframes(nframes)
+    modified.writeframes(original_frames)
+
+finally:
+    orginal.close()
+    modified.close()
