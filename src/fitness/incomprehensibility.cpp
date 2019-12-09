2e4db9a404abec9ca3533b25ecb9ed2c177c2a15
marunero <j0070ak@kaist.ac.kr>
2019-12-09 19:20:34 +0900

    Fix incomprehensibility

--- src/fitness/incomprehensibility.cpp
+++ src/fitness/incomprehensibility.cpp
@@ -15,7 +15,7 @@
 using namespace std;
 
 Incomprehensibility::Incomprehensibility(std::vector<Wav> &originals)
-    : originals(originals), _fitness(-1) {}
+    : originals(originals), _fitness(0), min_fitness(-1) {}
 
 bool Incomprehensibility::evaluateFitness(int sampleRate, Wav &original, Wav &modified)
 {
@@ -92,15 +92,15 @@ bool Incomprehensibility::evaluateFitness(int sampleRate, Wav &original, Wav &mo
 
   fitness = sqrt((float)verticalPath / (float)totalPath);
 
-  if (this->_fitness < 0){
-    this->_fitness = fitness;
+  if (this->min_fitness < 0){
+    this->min_fitness = fitness;
   }
   else{
-    if (this->_fitness > fitness){
-      this->_fitness = fitness;
+    if (this->min_fitness > fitness){
+      this->min_fitness = fitness;
     }
   }
-  
+
   /* free memory for DTW matrix */
   for (int i = 0; i < numRows; ++i)
   {
@@ -119,6 +119,8 @@ bool Incomprehensibility::evaluate(int sampleRate, Wav &modified)
     if (!success)
       return false;
   }
+  this->_fitness = this->min_fitness;
+  this->min_fitness = -1;
   return true;
 }
 
