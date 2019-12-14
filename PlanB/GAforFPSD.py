0458dff1a4c8f7c40edf18c9f6cf2379ccf245e7
Taeo <taeohy@kaist.ac.kr>
2019-12-15 05:57:58 +0900

    Add selection

--- PlanB/GAforFPSD.py
+++ PlanB/GAforFPSD.py
@@ -91,15 +91,20 @@
 ########################################################################################################################################################################################
 ########################################################################################################################################################################################
 import os
+import shutil
 import wave
 import random
 
 
-def ReadOriginalFiles(Path):
-    file_list_wav = [file for file in os.listdir(Path) if file.endswith(".wav")]
+def ReadOriginalFiles(SrcPath, DesPath):
+    
+    file_list_wav = [file for file in os.listdir(SrcPath) if file.endswith(".wav")]
 
     print ("file_list_wav: {}".format(file_list_wav))
 
+    for file in file_list_wav:
+        shutil.copy(SrcPath + file, DesPath)
+    
     return file_list_wav
 
 
@@ -111,7 +116,7 @@ def CrossoverTwoFiles(Path, FileNameToCrossover1, FileNameToCrossover2, Generati
         FileToCrossover1 = wave.open(Path + FileNameToCrossover1, 'rb')
         FileToCrossover2 = wave.open(Path + FileNameToCrossover2, 'rb')
         CrossoverFile1 = wave.open('./modified/' + str(Generation) + '/' + FileNameToCrossover1, 'wb')
-        CrossoverFile2 = wave.open('./modified/' + str(Generation) + '/' + FileNameToCrossover2, 'wb')
+        CrossoverFile2 = wave.open('./modified/' + str(Generation) + '/' + '_'+ FileNameToCrossover2, 'wb')
 
         nchannels1, sampwidth1, framerate1, nframes1, comptype1, compname1 = FileToCrossover1.getparams()
         nchannels2, sampwidth2, framerate2, nframes2, comptype2, compname2 = FileToCrossover2.getparams()
@@ -148,13 +153,52 @@ def CrossoverTwoFiles(Path, FileNameToCrossover1, FileNameToCrossover2, Generati
 
 
 def Crossover(Path, Pool, Generation):
+
     for file in Pool:
         CrossoverTwoFiles(Path, file, random.choice(Pool), Generation)
 
 
+def CalculateFitness_Sum(File1, File2):
+    try:
+        FileToCompare1 = wave.open(File1, 'rb')
+        FileToCompare2 = wave.open(File2, 'rb')
 
-Pool = ReadOriginalFiles("../audio/original/")
-Crossover('../audio/original/', Pool, 0)
+        nframes1 = FileToCompare1.getnframes()
+        nframes2 = FileToCompare2.getnframes()
 
-for i in range(100):
+        if nframes1 != nframes2:
+            print("Error : Different nframes!")
+
+        FileToCompare1_Frames = FileToCompare1.readframes(nframes1)
+        FileToCompare2_Frames = FileToCompare2.readframes(nframes1)
+
+        Difference = 0
+        for i in range(nframes1):
+            Difference += abs(FileToCompare1_Frames[i]-FileToCompare2_Frames[i])
+
+    finally:
+        FileToCompare1.close()
+        FileToCompare2.close()
+
+        return Difference
+
+
+for i in range(3):
+    shutil.rmtree('modified/'+str(i))
+    os.makedirs('modified/'+str(i))
+
+InitalPool = ReadOriginalFiles('../audio/original/', './modified/0/')
+
+for i in range(3):
+    Pool = os.listdir('./modified/'+str(i)+'/')
     Crossover('./modified/'+str(i)+'/', Pool, i+1)
+
+    for file in os.listdir('./modified/'+str(i)+'/'):
+        if file.startswith('_'):
+            FileToCompare1 = './modified/'+str(i)+'/'+file
+            FileToCompare2 = './modified/'+str(i)+'/'+file.lstrip('_')
+            if CalculateFitness_Sum('../audio/original/'+file.lstrip('_'), FileToCompare1) < CalculateFitness_Sum('../audio/original/'+file.lstrip('_'), FileToCompare2):
+                os.remove(FileToCompare1)
+            else:
+                os.remove(FileToCompare2)
+                os.rename(FileToCompare1, FileToCompare2)
